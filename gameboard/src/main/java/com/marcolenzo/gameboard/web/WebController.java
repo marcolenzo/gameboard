32f7c0ea9c4837f6a2f400e7720808d0f52d870a
Marco Lenzo <lenzo.marco@gmail.com>
2016-02-03 23:00:10 +0100

    Create landing page

--- gameboard/src/main/java/com/marcolenzo/gameboard/web/WebController.java
+++ gameboard/src/main/java/com/marcolenzo/gameboard/web/WebController.java
@@ -3,12 +3,22 @@ package com.marcolenzo.gameboard.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Main Controller.
+ * @author Marco Lenzo <lenzo.marco@gmail.com>
+ *
+ */
 @Controller
 public class WebController {
 
 	@RequestMapping(value = "login")
-	public String index() {
+	public String login() {
 		return "login";
+	}
+
+	@RequestMapping(value = "dashboard")
+	public String dashboard() {
+		return "dashboard";
 	}
 
 }
