0ebe8213a589554c392027240381054c8e3ec7b9
Marco Lenzo <lenzo.marco@gmail.com>
2016-02-06 11:12:45 +0100

    Find gameboards by user

--- gameboard-api/src/main/java/com/marcolenzo/gameboard/api/UserController.java
+++ gameboard-api/src/main/java/com/marcolenzo/gameboard/api/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Lists;
@@ -43,7 +42,7 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/api/user", method = RequestMethod.GET, params = { "nicknameOnly" })
-	public List<String> getNicknames(@RequestParam(value = "query", required = false) Boolean nicknameOnly) {
+	public List<String> getNicknames() {
 		List<User> users = repository.findAll();
 		List<String> nicknames = Lists.newArrayList();
 		for (User user : users) {
