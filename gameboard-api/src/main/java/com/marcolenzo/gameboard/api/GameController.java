1d9acf3cd5d70745cf709d2f35ab698884b000f6
Marco Lenzo <lenzo.marco@gmail.com>
2016-02-17 17:16:04 +0100

    Use rating services in game controller

--- gameboard-api/src/main/java/com/marcolenzo/gameboard/api/GameController.java
+++ gameboard-api/src/main/java/com/marcolenzo/gameboard/api/GameController.java
@@ -2,7 +2,6 @@ package com.marcolenzo.gameboard.api;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.validation.Valid;
@@ -18,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Sets;
+import com.marcolenzo.gameboard.api.services.RatingServices;
 import com.marcolenzo.gameboard.commons.model.Board;
-import com.marcolenzo.gameboard.commons.model.BoardPlayer;
 import com.marcolenzo.gameboard.commons.model.ResistanceGame;
 import com.marcolenzo.gameboard.commons.repositories.BoardRepository;
 import com.marcolenzo.gameboard.commons.repositories.ResistanceGameRepository;
@@ -44,6 +43,9 @@ public class GameController {
 	@Autowired
 	private BoardRepository boardRepository;
 
+	@Autowired
+	private RatingServices ratingServices;
+
 	@RequestMapping(value = "/api/game", method = RequestMethod.GET, params = { "boardId" })
 	public List<ResistanceGame> getGamesByBoardId(@RequestParam(value = "boardId", required = true) String boardId) {
 		return repository.findByBoardId(boardId);
@@ -58,7 +60,7 @@ public class GameController {
 	public ResistanceGame createGame(@Valid @RequestBody ResistanceGame game) {
 		LOGGER.info("Creating new game for board {}", game.getBoardId());
 		Board board = boardRepository.findOne(game.getBoardId());
-		return rateGame(game, board);
+		return ratingServices.rateGame(game, board);
 	}
 
 	@RequestMapping(value = "/api/game/test", method = RequestMethod.GET)
@@ -73,93 +75,6 @@ public class GameController {
 		return game;
 	}
 
-	private ResistanceGame rateGame(ResistanceGame game, Board board) {
-		Set<BoardPlayer> resistance = Sets.newHashSet();
-		Set<BoardPlayer> spies = Sets.newHashSet();
-		for (String userId : game.getPlayers()) {
-			if (game.getSpies().contains(userId)) {
-				spies.add(board.getPlayersMap().get(userId));
-			}
-			else {
-				resistance.add(board.getPlayersMap().get(userId));
-			}
-		}
-
-		// Compute average resistance ELO
-		double resistanceElo = 0;
-		int count = 0;
-		for (BoardPlayer user : resistance) {
-			resistanceElo += user.getElo();
-			count++;
-		}
-		resistanceElo = resistanceElo / count;
-
-		// Compute average spies ELO
-		double spiesElo = 0;
-		count = 0;
-		for (BoardPlayer user : spies) {
-			spiesElo += user.getElo();
-			count++;
-		}
-		spiesElo = spiesElo / count;
-		
-		game.setResistanceElo((int) resistanceElo);
-		game.setSpiesElo((int) spiesElo);
-
-		// Transform elos
-		resistanceElo = (int) Math.pow(10, resistanceElo / 400);
-		spiesElo = (int) Math.pow(10, spiesElo / 400);
-
-		for (BoardPlayer user : resistance) {
-			int eloVariation = ratePlayer(user, game.getBoardId(), game.getResistanceWin() ? 1 : 0, spiesElo, false);
-			game.getEloVariations().put(user.getUserId(), eloVariation);
-		}
-
-		for (BoardPlayer user : spies) {
-			int eloVariation = ratePlayer(user, game.getBoardId(), game.getResistanceWin() ? 0 : 1, resistanceElo, true);
-			game.getEloVariations().put(user.getUserId(), eloVariation);
-		}
-
-		boardRepository.save(board);
-		return repository.save(game);
-	}
-	
-	/**
-	 * 
-	 * @param user
-	 * @param boardId
-	 * @param score
-	 * @param opponentsElo
-	 * @param isSpy
-	 * @return ELO variation
-	 */
-	private Integer ratePlayer(BoardPlayer user, String boardId, int score, double opponentsElo, boolean isSpy) {
-		Integer elo = user.getElo();
-		// Transform user's elo
-		double tranformedElo = (int) Math.pow(10, (double) elo / 400);
-		double expectedScore = tranformedElo / (tranformedElo + opponentsElo);
-		Integer finalElo = (int) (elo + (32 * (score - expectedScore)));
-
-		user.setElo(finalElo);
-		user.setMatchesPlayed(user.getMatchesPlayed() + 1);
-		if (isSpy) {
-			user.setMatchesPlayedAsSpy(user.getMatchesPlayedAsSpy() + 1);
-			if (score == 1) {
-				user.setMatchesWonAsSpy(user.getMatchesWonAsSpy() + 1);
-				user.setMatchesWon(user.getMatchesWon() + 1);
-			}
-		}
-		else {
-			user.setMatchesPlayedAsResistance(user.getMatchesPlayedAsResistance() + 1);
-			if (score == 1) {
-				user.setMatchesWonAsResistance(user.getMatchesWonAsResistance() + 1);
-				user.setMatchesWon(user.getMatchesWon() + 1);
-			}
-		}
-
-		return finalElo - elo;
-
-	}
 
 
 
