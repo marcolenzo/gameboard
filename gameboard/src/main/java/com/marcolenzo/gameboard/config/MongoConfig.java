0b85613339ec25abef4a9c114a7ff32299118638
Marco Lenzo <lenzo.marco@gmail.com>
2025-01-31 15:42:58 +0100

    Upgrade to latest Spring Boot version
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/marcolenzo/gameboard?shareId=XXXX-XXXX-XXXX-XXXX).

--- gameboard/src/main/java/com/marcolenzo/gameboard/config/MongoConfig.java
+++ gameboard/src/main/java/com/marcolenzo/gameboard/config/MongoConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 /**
@@ -21,12 +20,12 @@ public class MongoConfig {
 	@Value("mongo.host")
 	private String mongoHost;
 
-	public @Bean Mongo mongo() throws UnknownHostException {
+	public @Bean MongoClient mongoClient() throws UnknownHostException {
 		return new MongoClient(mongoHost);
 	}
 
 	public @Bean MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongo(), "gameboard");
+		return new MongoTemplate(mongoClient(), "gameboard");
 	}
 
 }
--- gameboard/src/main/java/com/marcolenzo/gameboard/controllers/GameboardErrorController.java
+++ gameboard/src/main/java/com/marcolenzo/gameboard/controllers/GameboardErrorController.java
@@ -1,6 +1,5 @@
 package com.marcolenzo.gameboard.controllers;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,15 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
-public class GameboardErrorController implements ErrorController {
+public class GameboardErrorController {
 
 	@RequestMapping(value = "/error")
 	public String error() {
-		return "index";
-	}
-
-	@Override
-	public String getErrorPath() {
 		return "index";
 	}
 
